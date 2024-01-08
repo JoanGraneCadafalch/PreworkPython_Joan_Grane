a5ac610038e4fc7cce57b0ee72e84937e30bbd74
JoanGraneCadafalch <joan.grane.cadafalch@gmail.com>
2024-01-08 12:35:06 +0100

    feat: Prework Python exercises - Joan Grane

---
+++ ejercicio1.py
@@ -0,0 +1,16 @@
+# 1. Ejercicio: Crea una variable llamada nombre y asígnale tu nombre como valor. Luego, imprime la variable.
+nombre = 'Joan'
+print('Resolución 1.1:',nombre)
+# 2. Ejercicio: Crea dos variables, a y b , y asígnales los valores 5 y 10 respectivamente. Luego, imprime la suma de a y b .
+a = 5
+b = 10
+print('Resolución 1.2:',a+b)
+# 3. Ejercicio: Calcula el área de un triángulo con base 10 y altura 5.
+base = 10
+altura = 5
+area = base * altura / 2
+print('Resolución 1.3: El area es',area)
+# 4. Ejercicio: Calcula el resto de dividir 17 entre 3.
+division = 17 / 3
+resto = division - int(division)
+print('Resolución 1.4:',resto)
